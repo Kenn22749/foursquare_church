09562753fa8b6f3f0fa4e91005e3eb39841ce9e3
unknown <saldivarkenn2@gmail.com>
2026-04-28 09:38:45 +0800

    Remove temporary admin creation code

--- main/apps.py
+++ main/apps.py
@@ -4,20 +4,3 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
-
-    def ready(self):
-        import os
-
-        if os.environ.get("RENDER"):
-            try:
-                from django.contrib.auth.models import User
-
-                if not User.objects.filter(username="Admin22church").exists():
-                    User.objects.create_superuser(
-                        username="Admin22church",
-                        email="admin22fqchurch@gmail.com",
-                        password="admin789123"
-                    )
-                    print("Temporary Admin22church account created.")
-            except Exception as e:
-                print("Temporary admin creation skipped:", e)

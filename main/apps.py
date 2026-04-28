32efc7f85f21e2f37b11c2856b92cc0737c82de6
unknown <saldivarkenn2@gmail.com>
2026-04-28 09:35:05 +0800

    Fix temporary admin account creation

--- main/apps.py
+++ main/apps.py
@@ -12,12 +12,12 @@ class MainConfig(AppConfig):
             try:
                 from django.contrib.auth.models import User
 
-                if not User.objects.filter(username="admin").exists():
+                if not User.objects.filter(username="Admin22church").exists():
                     User.objects.create_superuser(
                         username="Admin22church",
                         email="admin22fqchurch@gmail.com",
                         password="admin789123"
                     )
-                    print("Temporary admin account created.")
+                    print("Temporary Admin22church account created.")
             except Exception as e:
                 print("Temporary admin creation skipped:", e)
